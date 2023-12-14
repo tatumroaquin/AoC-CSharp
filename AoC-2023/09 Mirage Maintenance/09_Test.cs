3fd41b1052000a9557fad4aa3656ca6ac611a9e4
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-12-14 19:21:00 +1100

    update 2032 day 9

--- AoC-2023/09 Mirage Maintenance/09_Test.cs
+++ AoC-2023/09 Mirage Maintenance/09_Test.cs
@@ -4,15 +4,11 @@ public class AoC_2023_Day_09 {
   public static void Test() {
     List<List<long>> example = InputParser.ParseInput("09 Mirage Maintenance/Data/ExampleInput.in");
     List<List<long>> sequences = InputParser.ParseInput("09 Mirage Maintenance/Data/PuzzleInput.in");
-    // foreach (var sequence in sequences) {
-    //   foreach (long num in sequence) {
-    //     Console.Write($"{num} ");
-    //   }
-    //   Console.WriteLine();
-    // }
     long part1 = Part1.Solution(sequences);
-    long part2 = Part2.Solution2(sequences);
+    long part2a = Part2A.Solution(sequences);
+    long part2b = Part2B.Solution(sequences);
     Console.WriteLine($"Part 1 Answer: {part1}");
-    Console.WriteLine($"Part 2 Answer: {part2}");
+    Console.WriteLine($"Part 2A Answer: {part2a}");
+    Console.WriteLine($"Part 2B Answer: {part2b}");
   }
 }
--- AoC-2023/09 Mirage Maintenance/Part2B.cs
+++ AoC-2023/09 Mirage Maintenance/Part2B.cs
@@ -1,11 +1,10 @@
 namespace AoC_2023.Day_09;
 
-public class Part2 {
-  public static long Solution1(List<List<long>> sequences) {
+public class Part2B {
+  public static long Solution(List<List<long>> sequences) {
     // store the last numbers in every sequence
     long lastNumsTotal = 0;
     foreach (List<long> sequence in sequences) {
-      sequence.Reverse(); // just reverse the input
       List<long> curr = sequence;
       while (!curr.All(num => num == 0)) {
         lastNumsTotal += curr.Last();
@@ -18,12 +17,13 @@ public class Part2 {
   private static List<long> GetNextSequence(List<long> sequence) {
     if (sequence.Count < 2) return new List<long> { 0 };
     var res = new List<long>();
-    for (int i = 0; i < sequence.Count - 1; i++) {
-      long curr = sequence[i];
-      long next = sequence[i+1];
 
-      long diff = next - curr;
-      res.Add(diff);
+    for (int i = sequence.Count - 1; i > 0; i--) {
+      long prev = sequence[i-1];
+      long curr = sequence[i];
+
+      long diff = curr - prev;
+      res.Insert(0, diff);
     }
     return res;
   }
